afc2c9adc2fc12101d66411f39548af8af82fffd
Denys-Sh <arrowssilvers@gmail.com>
2017-07-04 17:42:52 +0300

    home_10/9

---
+++ style.js
@@ -0,0 +1,3 @@
+$("#message").attr({style:"border:1px solid brown; width:28%; color:yellow;"});   
+$("#to").attr({style:"border:2px solid pink; width:30%;  color:red;"});  
+$("#from").attr({style:"border:1px solid green; width:25%; color:blue;"});   
